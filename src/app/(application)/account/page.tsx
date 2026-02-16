9f08ba7307e6fbc9c0ee8285d610e9a8b1cd2271
Saikat Sardar <saikatsardar5@gmail.com>
2026-02-16 20:36:12 +0530

    feat: add StudentProfile model and shadcn/ui components

---
+++ src/app/(application)/account/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Account - Institute OS",
+  description: "Manage your Institute OS account",
+};
+
+const page = () => {
+  return <></>;
+};
+
+export default page;
